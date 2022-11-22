09753ed963612d81cd774c85e668207c56182d85
fred <daktarispacetime@gmail.com>
2022-11-22 14:07:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+    function clickAlert() {
+        alert('I was clicked!');
+    }
 }
